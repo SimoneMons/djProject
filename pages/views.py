92862608b7e80c981bb71637876fe88032c5e6a8
SimoneMons <34129550+SimoneMons@users.noreply.github.com>
2024-04-17 11:46:13 +0200

    v1_1704

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 from .models import student
 
 def home_page_view(request):
     return render(request, 'home.html', locals())
 
+@login_required
 def signup_student(request):
     return render(request,'signup_student.html',{'msg':'Student Signup'})
 
-
+@login_required
 def signup_student_output(request):
     en=student(name=request.POST.get('name'),cl=request.POST.get('class'),
      mark=request.POST.get('mark'),gender=request.POST.get('gender'))
